76a9af85f2255550c59b5da9ebdb46f691fa8a09
Edison Calderon <edison_c2h@hotmail.com>
2019-05-19 20:23:19 -0500

    Sidebar Component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
+import './Sidebar.css';
 
 
 const Sidebar = () => {
     return (
-        <nav className="col-md-2 d-none d-md-block bg-light sidebar">
-            <div className="sidebar-sticky">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/login" className="nav-link">Login</Link>
-            </div>
-        </nav>
+        <Menu>
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/login" className="nav-link">Login</Link>
+        </Menu>
+ 
     )
 }
 
